c49ede5902148a93e6e8889281b2464cad217939
vijeshsrimal14 <vijeshsrimal15@gmail.com>
2022-08-22 12:57:00 +0530

    Edit1

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-let firstVar = 10
-let secondVar = 11
-console.log(firstVar + secondVar)
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let MessageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("cards-el")
+function startGame(){
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    MessageEl.textContent = message
+}
