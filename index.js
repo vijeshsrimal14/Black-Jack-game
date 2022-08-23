0e920a15e2ec9da68d04e85bf0b6d1cb1cfcb999
vijeshsrimal14 <vijeshsrimal15@gmail.com>
2022-08-23 21:40:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let cards = [firstCard, secondCard]
 
 let MessageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("cards-el")
 function startGame(){
-    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardEl.textContent = "Cards: "
+    for(let i = 0;i<cards.length;i++){
+        cardEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -23,5 +27,7 @@ function startGame(){
     MessageEl.textContent = message
 }
 function newCard(){
-    
+    let card = 7
+    cards.push(card)
+    startGame()
 }
